7317dd1ab0d96c5f547623ccdccb06453df16e89
mav3rick177 <y.radouani929@gmail.com>
2021-03-25 02:40:03 +0100

    Fix default.blade error

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -8,13 +8,13 @@
                 </li>
             @else
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator['previousUrl'] ?: }}" rel="prev" aria-label="Previous">&lsaquo; Previous</a>
+                    <a class="page-link" href="{{ $paginator->previousUrl }}" rel="prev" aria-label="Previous">&lsaquo; Previous</a>
                 </li>
             @endif
             {{-- Next Page Link --}}
             @if ($paginator->hasNext)
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator['nextUrl'] ?: }}" rel="next" aria-label="Next">Next &rsaquo;</a>
+                    <a class="page-link" href="{{ $paginator->nextUrl }}" rel="next" aria-label="Next">Next &rsaquo;</a>
                 </li>
             @else
                 <li class="page-item disabled" aria-disabled="true" aria-label="Next">
